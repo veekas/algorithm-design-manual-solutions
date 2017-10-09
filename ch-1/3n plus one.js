02ebf6e7f0092e7df85a583787f364f867857ecb
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-10-09 00:13:41 -0400

    initial commit, non-working solution file for 3n+1 problem (counter doesn't reset)

---
+++ ch-1/3n plus one.js
@@ -0,0 +1,33 @@
+// counter is not resetting
+
+const getToOneFrom = (function () {
+  let counter = 0;
+  return function (num) {
+    if (num < 1) return 'error';
+    while (num !== 1) {
+      counter++;
+      if (num % 2 === 1) {
+        // console.log(num, counter);
+        return getToOneFrom(3 * num + 1);
+      } else {
+        // console.log(num, counter);
+        return getToOneFrom(num / 2);
+      }
+    }
+    counter++;
+    // console.log(num, counter);
+    return counter;
+  };
+})();
+
+function getToOneForEachBetween(first, last) {
+  let maxCount = 0;
+
+  for (let i = first; i <= last; i++) {
+    let thisCount = getToOneFrom(i);
+    if (thisCount > maxCount) maxCount = thisCount;
+  }
+
+  return `${first} ${last} ${maxCount}`;
+
+}
