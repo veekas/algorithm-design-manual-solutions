6de44dc84a7dd3815ff97890938baaaec0bd6d85
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-10-09 20:43:58 -0400

    refactored 3n+1

--- ch-1/3n plus one.js
+++ ch-1/3n plus one.js
@@ -1,43 +1,46 @@
-// counter is not resetting
-
-// const getToOneFrom = (function () {
-//   let counter = 0;
-//   return function (num) {
-//     if (num < 1) return 'error';
-//     counter++;
-//     if (num === 1) {
-//       let counter2 = counter; // pretty hacky
-//       counter = 0;
-//       return counter2;
-//     }
-//     if (num % 2 === 1) {
-//       return getToOneFrom(3 * num + 1);
-//     } else {
-//       return getToOneFrom(num / 2);
-//     }
-//   };
-// })();
-
 const getToOneFrom = function (num) {
   if (num < 1) return 'error';
-  if (num === 1) {
-    return 1;
-  }
-  if (num % 2 === 1) {
-    return 1 + getToOneFrom(3 * num + 1);
-  } else {
-    return 1 + getToOneFrom(num / 2);
-  }
+  if (num === 1) return 1;
+  return num % 2 === 1
+    ? 1 + getToOneFrom(3 * num + 1)
+    : 1 + getToOneFrom(num / 2);
 };
 
 function maxIterationsToReachOneBetween(first, last) {
   let maxCount = 0;
-
   for (let i = first; i <= last; i++) {
     let thisCount = getToOneFrom(i);
     if (thisCount > maxCount) maxCount = thisCount;
   }
-
   return `${first} ${last} ${maxCount}`;
-
 }
+
+// using counter
+
+  // const getToOneFrom = (function () {
+  //   let counter = 0;
+  //   return function (num) {
+  //     if (num < 1) return 'error';
+  //     counter++;
+  //     if (num === 1) {
+  //       let counter2 = counter; // pretty hacky
+  //       counter = 0;
+  //       return counter2;
+  //     }
+  //     if (num % 2 === 1) {
+  //       return getToOneFrom(3 * num + 1);
+  //     } else {
+  //       return getToOneFrom(num / 2);
+  //     }
+  //   };
+  // })();
+
+// using array.reduce:
+
+  // function maxIterationsToReachOneBetween(first, last) {
+  //   let maxCount = 0;
+  //   let countArray = [];
+  //   for (let i = first; i <= last; i++) { countArray.push(getToOneFrom(i)); }
+  //   maxCount = countArray.reduce((a, b) => Math.max(a, b));
+  //   return `${first} ${last} ${maxCount}`;
+  // }
